3c9b62226e43004d3471aca23874aaef81e91758
EC2 Default User <ec2-user@ip-172-31-28-245.ap-northeast-1.compute.internal>
2014-04-07 10:45:35 +0000

    eee

---
+++ wp-cli/recipes/deploy.rb
@@ -0,0 +1,46 @@
+require "net/http"
+require "uri"
+
+wpdir = "/srv/www/wordpress/current"
+dbname = node[:mysql][:dbname]
+dbuser = node[:mysql][:dbuser]
+dbpass = node[:mysql][:dbpass]
+dbhost = node[:mysql][:dbhost]
+wp_admin_email = node[:wordpress_custom][:wp_admin_email]
+
+execute "wp configure" do
+   command "wp core config --dbname=#{dbname} --dbuser=#{dbuser} --dbpass=#{dbpass} --dbhost=#{dbhost}"
+   cwd "#{wpdir}"
+   user "deploy"
+   not_if { File.exists?("#{wpdir}/wp-config.php") }
+   action :run
+end
+
+
+uri = URI.parse("http://169.254.169.254/latest/meta-data/public-hostname")
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+
+public_hostname = response.body
+
+execute "db create" do
+   command "wp db create"
+   cwd "#{wpdir}"
+   user "deploy"
+   action :run
+   not_if do
+      require 'rubygems'
+      Gem.clear_paths
+      require 'mysql'
+      m = Mysql.new("mydb.cm4rmjkwcnlv.ap-northeast-1.rds.amazonaws.com", "awsuser", "mypassword")
+      m.list_dbs.include?("mydb")
+end
+
+execute "wp deploy" do
+   command "wp db create && wp core install --url=#{public_hostname} --title=Test --admin_name=admin --admin_password=admin --admin_email=#{wp_admin_email}"
+   cwd "#{wpdir}"
+   user "deploy"
+   action :run
+   not_if "sudo -u deploy wp core is-installed --path=#{wpdir}"
+end
